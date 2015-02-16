d505dc6ed73635679ca96970d3d045611f847855
Sebastian Cheung <sebastian_cheung@yahoo.com>
2015-02-16 14:15:34 +0000

    Second commit - changed css

--- roleswap/config.py
+++ roleswap/config.py
@@ -96,9 +96,9 @@ IMG_UPLOAD_URL = '/static/uploads/'
 #APP_THEME = "flatly.css"
 #APP_THEME = "journal.css"
 #APP_THEME = "readable.css"
-#APP_THEME = "simplex.css"
+APP_THEME = "simplex.css"
 #APP_THEME = "slate.css"   
 #APP_THEME = "spacelab.css"
 #APP_THEME = "united.css"
-APP_THEME = "yeti.css"
+# APP_THEME = "yeti.css"
 
